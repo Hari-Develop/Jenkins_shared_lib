c8f87e5fbac1bf8cb5fa8aced79e90b127fb5543
Harinadha Reddy <hreddy@OSBIndia.COM>
2025-01-25 16:17:40 +0530

    folder added

---
+++ vars/pipelineDecission.groovy
@@ -0,0 +1,17 @@
+def decidepipeline(Map configMap){
+    application = application.configMap.get("application")
+    switch("application") {
+        case 'nodejsVM':
+            nodejsVM(configMap)
+        break
+        case 'JavajsVM':
+            javajsVM(configMap)
+        break
+        case 'nodeEKS':
+            nodeEKS(configMap)
+        break
+        default:
+            error "application is not  recognised"
+            break
+    }
+}
